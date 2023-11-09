95a525a3ad65fd62b42cb1d3c281eb508fd03df1
yuzuki <y.yamahata.sys23@morijyobi.ac.jp>
2023-11-09 11:06:35 +0900

    README.mdの追加

--- Bmi.java
+++ Bmi.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
-public class Main {
+public class Bmi {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		for(int n = 0;n < 2; n++){	
+		for(int n = 0;n < 2; n++){
+
+			System.out.println("身長");
 		int height =Integer.parseInt(sc.nextLine());
+
+			System.out.println("体重");
 			int weight =Integer.parseInt(sc.nextLine());
+
+			System.out.println("BMI");
 			double BMI =(double)(height / (weight*weight));
 		System.out.println(BMI);
 	}
