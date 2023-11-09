0b3d4ce91d4125dd87fdf28b4b94f48599e8ac56
yuzuki <y.yamahata.sys23@morijyobi.ac.jp>
2023-11-09 10:42:33 +0900

    Main.javaの機能変更

--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-			int height =Integer.parseInt(sc.nextLine());
+		for(int n = 0;n < 2; n++){	
+		int height =Integer.parseInt(sc.nextLine());
 			int weight =Integer.parseInt(sc.nextLine());
 			double BMI =(double)(height / (weight*weight));
 		System.out.println(BMI);
+	}
 	}
 }
