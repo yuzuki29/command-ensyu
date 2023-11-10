023795fb0b7f5afceab51411605870d850bd3bf3
YamahataYuzuki <146398990+yuzuki29@users.noreply.github.com>
2023-11-10 12:28:16 +0900

    Bmi.javaの機能修正

--- Bmi.java
+++ Bmi.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 public class Bmi {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		for(int n = 0;n < 2; n++){
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        for(int n = 0;n < 2; n++){
 
-			System.out.println("身長");
-		int height =Integer.parseInt(sc.nextLine());
-
-			System.out.println("体重");
-			int weight =Integer.parseInt(sc.nextLine());
-
-			System.out.println("BMI");
-			double BMI =(double)(height / (weight*weight));
-		System.out.println(BMI);
-	}
-	}
+            System.out.println("身長");
+            int height =Integer.parseInt(sc.nextLine());
+            double dh =height;
+            System.out.println("体重");
+            int weight =Integer.parseInt(sc.nextLine());
+            double dw = weight;
+            System.out.println("BMI");
+            double BMI =(dh / (dw*dw));
+            System.out.println(String.format("%.2f",BMI));
+        }
+    }
 }
